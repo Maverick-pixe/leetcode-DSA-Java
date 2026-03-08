e4f02e68831c9ddf442439e4698d2786ccee9c87
Debashanand C <debashc105@gmail.com>
2026-03-08 15:27:50 +0000

    Optimize findMaxAverage method implementation
    Refactor to calculate maximum average without nested loops.

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,14 +1,16 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         int n = nums.length;
-        int maxSum =Integer.MIN_VALUE;
-        for(int i = 0;i<=n-k;i++){
-            int sum = 0;
-            for(int j = i;j<i+k;j++){
-                sum += nums[j];
-            }
+        int sum = 0;
+        for(int i = 0;i<k;i++){
+             sum += nums[i];
+        }
+       int  maxSum = sum;
+        for(int i= k;i<n;i++){
+            sum -= nums[i];
+            sum+= nums[i-k];
             maxSum = Math.max(maxSum,sum);
         }
-        return (double) maxSum/k;
+        return (double) maxSum;
     }
 }
