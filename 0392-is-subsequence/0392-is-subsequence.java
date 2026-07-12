63a9b000ff8e404c33b3cdbee8b28f27e06da7fc
Debashanand C <debashc105@gmail.com>
2026-07-13 00:14:39 +0530

    Time: 2 ms (72.55%), Space: 42.9 MB (44.42%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-    int i=0,j=0;
-    while(i<s.length()&&j<t.length()){
-        if(s.charAt(i)== t.charAt(j)){  //O(n+m) The pointer j moves through every character of t, and i moves at most n times.
-            i++;
+        int i =0, j =0;
+        while(i<s.length()&&j<t.length()){
+            if(s.charAt(i)== t.charAt(j)){
+                i++;
+            }
+        j++;
         }
-    j++;
+    return i == s.length();
     }
-    return i ==s.length();   // If i is equal to the length of s, all characters of s were found in t
-    }
-    }
+}
