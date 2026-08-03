10d68d55714d34ac10792c51a19caf1be280b0d4
Debashanand C <debashc105@gmail.com>
2026-08-03 23:35:32 +0530

    Time: 6 ms (99.96%), Space: 46.9 MB (76.22%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.java
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -1,66 +1,92 @@
 class MyLinkedList {
-
-private class Node{
-    int val;
-    Node next;
-     
-     Node(int val){
-        this.val = val;
-     }
-}
-    private Node dummy;
-    private int size;
-    public MyLinkedList() {
-         dummy = new Node(0);
-         size = 0;
-    }
-    
-    public int get(int index) {
-        if(index < 0 || index >= size ) return -1;
-        Node curr = dummy.next;
-        for(int i  = 0;i<index;i++){
-            curr = curr.next;
+    private class Node {
+        int val;
+        Node prev;
+        Node next;
+        Node(int val) {
+            this.val = val;
         }
-        return curr.val;
     }
-    
+
+    private Node headSentinel;
+    private Node tailSentinel;
+    private int size;
+
+    public MyLinkedList() {
+        headSentinel = new Node(0);
+        tailSentinel = new Node(0);
+        headSentinel.next = tailSentinel;
+        tailSentinel.prev = headSentinel;
+        size = 0;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) return -1;
+        Node node = getNodeAt(index);
+        return node.val;
+    }
+
     public void addAtHead(int val) {
-        addAtIndex(0,val);
+        Node newNode = new Node(val);
+        Node after = headSentinel.next;
+        headSentinel.next = newNode;
+        newNode.prev = headSentinel;
+        newNode.next = after;
+        after.prev = newNode;
+        size++;
     }
-    
+
     public void addAtTail(int val) {
-        addAtIndex(size,val);
+        Node newNode = new Node(val);
+        Node before = tailSentinel.prev;
+        before.next = newNode;
+        newNode.prev = before;
+        newNode.next = tailSentinel;
+        tailSentinel.prev = newNode;
+        size++;
     }
-    
+
     public void addAtIndex(int index, int val) {
-      if(index < 0 || index > size ) return ;
-      Node temp = dummy;
-      for(int i = 0;i<index;i++){
-        temp = temp.next;
-      }
-      Node newNode = new Node(val);
-      newNode.next = temp.next;
-     temp.next = newNode;
-      size++;
+        if (index < 0 || index > size) return;
+        Node successor;
+        Node predecessor;
+        if (index == size) {
+            successor = tailSentinel;
+            predecessor = tailSentinel.prev;
+        } else {
+            successor = getNodeAt(index);
+            predecessor = successor.prev;
+        }
+        Node newNode = new Node(val);
+        newNode.prev = predecessor;
+        newNode.next = successor;
+        predecessor.next = newNode;
+        successor.prev = newNode;
+        size++;
     }
-    
+
     public void deleteAtIndex(int index) {
-         if(index < 0 || index >= size ) return ;
-         Node temp = dummy;
-         for(int i = 0;i<index;i++){
-            temp = temp.next;
-         }
-         temp.next = temp.next.next;
-         size--;
+        if (index < 0 || index >= size) return;
+        Node node = getNodeAt(index);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        size--;
+    }
+
+    // Helper: traverse from whichever end is closer
+    private Node getNodeAt(int index) {
+        Node node;
+        if (index < size / 2) {
+            node = headSentinel.next;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+        } else {
+            node = tailSentinel.prev;
+            for (int i = size - 1; i > index; i--) {
+                node = node.prev;
+            }
+        }
+        return node;
     }
 }
-
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
