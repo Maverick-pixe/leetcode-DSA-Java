27858fda12a2d9d8837440aae434bc6f8fab57a6
Debashanand C <debashc105@gmail.com>
2026-08-08 00:16:12 +0530

    Time: 0 ms (100%), Space: 46.7 MB (30.98%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,21 +11,24 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
+      ListNode slow = head;
+      ListNode fast = head;
 
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-            if(slow == fast){       // time complex is o(n);
-                slow = head;
-                while(slow != fast){
-                    slow= slow.next;
-                    fast = fast.next;
-                }
-            return slow;
-            }
-        }                          // space complex is o(1);
+ 
+        
+        if(slow == fast){
+            slow = head;
+            while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+        }
+        }
         return null;
     }
+
 }
