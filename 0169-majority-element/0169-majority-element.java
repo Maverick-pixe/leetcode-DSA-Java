a6e38a0b4d5d2882c8e25536a1e6198374e8bec9
Debashanand C <debashc105@gmail.com>
2026-08-29 01:37:21 +0530

    Time: 12 ms (37.54%), Space: 52.9 MB (72.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int thresHold = n/2;
-        int count =0;
+        int  n = nums.length;
+        int threshHold = n/2;
+        int count = 0;
         HashMap<Integer,Integer> map = new HashMap<>();
-       for(int num:nums){
-         count =  map.getOrDefault(num,0)+1;
-         map.put(num,count);
-         if(count> thresHold){
-            return num;
-         }
-       }
-return -1;
+        for(int num : nums){
+            count = map.getOrDefault(num,0)+1;
+            map.put(num,count);
+            if(count > threshHold){
+                return num;
+            }
+        }
+        return -1;
     }
 }
