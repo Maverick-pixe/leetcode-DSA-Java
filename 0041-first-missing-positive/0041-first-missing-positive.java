3ab475bff16e0e7bd0b42b3a5e331aa00e8fae97
Debashanand C <debashc105@gmail.com>
2026-09-02 00:59:06 +0530

    Time: 1 ms (99.96%), Space: 71.4 MB (38.48%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,18 +1,23 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        // Store all numbers in a hash set for O(1) lookup
-        Set<Integer> seen = new HashSet<>();
-        for (int num : nums) {
-            seen.add(num);
+        private void swap(int[] arr,int i ,int j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
 
-        // Check 1, 2, 3, ... until we find one that's missing
-        for (int i = 1; i <= nums.length + 1; i++) {
-            if (!seen.contains(i)) {
-                return i;
+        for(int i =0;i<n;i++){
+            while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]-1]){
+                swap(nums,i,nums[i] -1);
             }
         }
-
-        return nums.length + 1;
+        for(int i = 0;i<n;i++){
+            if(nums[i] != i+1){
+                return i+1;
+            }
+            
+        }
+        return n+1;
     }
 }
